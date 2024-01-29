149eb6c43d73af1edb05cf335ea568732a3d2704
killthebee <danny.developer@ro.ru>
2024-01-29 09:27:19 +0300

    init setup

---
+++ notes/misc.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+let mainBackgroundColor = UIColor(red: 45/255, green: 9/255, blue: 9/255, alpha: 1.0)
+let cellsBackgroundColor = UIColor(red: 233/255, green: 82/255, blue: 71/255, alpha: 1.0)
